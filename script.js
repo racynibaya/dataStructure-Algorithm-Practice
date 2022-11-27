bb3f1a562efd915e96ef9aca095a0bde0332934b
Racyn Ibaya <racynibaya@outlook.com>
2022-11-27 15:17:16 -0500

    Constant time 0(1) do things constantly

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ function findNemo(array) {
 }
 
 findNemo(large);
+
+// Constant Time - O(1)
+// No matter the boxes increase it only grabs one thing.
+const boxes = [0, 1, 2, 3, 4, 5, 6, 7];
+const compressFirstBox = function (boxes) {
+  console.log(boxes[0]);
+};
+
+compressFirstBox(boxes);
+
+const logFirstTwoBoxes = function (boxes) {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(1)
+};
+
+logFirstTwoBoxes(boxes); // O(2)
